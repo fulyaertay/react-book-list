bb2d4f2c04d196fe08bf8bc06359fc8b5bcc9af7
Fulya <fulya.ertay@gmail.com>
2022-12-31 23:51:40 +0300

    feat:bug fixes

--- src/context/BookContext.js
+++ src/context/BookContext.js
@@ -1,5 +1,5 @@
 import React,{createContext,useState} from 'react';
-
+import uuid from 'uuid/v1';
 export const BookContext=createContext();
 const BookContextProvider=(props)=>{
     const [books,setBooks]=useState([
@@ -15,15 +15,18 @@ const BookContextProvider=(props)=>{
 
         }
     ]);
-    const addBook=(title,author)={
-        setBooks([...books,{title,author,id:3}])
+    const addBook=(title,author)=>{
+        setBooks([...books,{title:title,author:author,id:uuid()}]);
+    }
+    const removeBook=(id)=>{
+        setBooks(books.filter(book=>book.id!=id));
     }
     return(
-        <BookContext.Provider value={{books}}>
+        <BookContext.Provider value={{books,addBook,removeBook}}>
             {props.children}
         </BookContext.Provider>
 
     )
 }
 
-export default BookContextProvider
+export default BookContextProvider;
