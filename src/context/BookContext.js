ba1da46512da0bb2d6cd1d55bf5da3de97cc866a
Fulya <fulya.ertay@gmail.com>
2022-12-31 23:40:54 +0300

    feat:add uuid

--- src/context/BookContext.js
+++ src/context/BookContext.js
@@ -1,10 +1,29 @@
-import React,{createContext} from 'react';
+import React,{createContext,useState} from 'react';
 
 export const BookContext=createContext();
-const BookContextProvider=()=>{
+const BookContextProvider=(props)=>{
+    const [books,setBooks]=useState([
+        {
+            title:'Dönüşüm',
+            author:'Franz Kafka',
+            id:1
+        },
+        {
+            title:'Hayavn Çiftliği',
+            author:'George Orwell',
+            id:2
+
+        }
+    ]);
+    const addBook=(title,author)={
+        setBooks([...books,{title,author,id:3}])
+    }
     return(
+        <BookContext.Provider value={{books}}>
+            {props.children}
+        </BookContext.Provider>
 
     )
 }
 
-export default BookContextProvider;
+export default BookContextProvider
